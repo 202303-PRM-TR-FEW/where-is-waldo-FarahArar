045cd6329f01c78df138fd798f817faf0bba76ef
Farah Arar <farah02arar@gmail.com>
2023-10-11 20:48:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const example = [
   ["A", "B", "A"]];
 
 const whereIsWaldo = (matrix) => {
+  for(let i=0;i<matrix.length;i++){
+    for(let j=0;j<matrix[i].length;j++){
+      if(matrix[i][j] !=='A')
+          return [i+1,j+1]
+    }
+  }
   
 }
